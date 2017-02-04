a18f08ebf2350e17b87407bbdd787c4aa239cfb3
Szymon Michalak <szymonmichalak@gmail.com>
2017-02-04 14:27:43 +0100

    Add more inputs of different types to our form

--- src/containers/App.js
+++ src/containers/App.js
@@ -9,36 +9,96 @@ class App extends Component {
     super(props)
     // Let's store the value of `name` in component's state
     this.state = {
-      name: ''
+      name: '',
+      email: '',
+      checkbox: '',
+      radio: 'option1',
+      date: ''
     }
     // Need to bind `this` to `handleInputChange` in order to be able to access `this` inside it
     this.handleInputChange = this.handleInputChange.bind(this)
   }
 
-  handleInputChange(event) {
-    // Get `value` from `onChange` event (it is triggered on input when user types in the input)
-    const { value } = event.target
-    // Save this value to our state
-    this.setState({
-      name: value
-    })
+  handleInputChange(inputName) {
+    return (event) => {
+      // Get `value` from `onChange` event (it is triggered on input when user types in the input)
+      const { value } = event.target
+      // Save this value to our state
+      this.setState({
+        [inputName]: value
+      })
+    }
   }
 
   render() {
     const alertMsg = this.state.name ? `Hi ${this.state.name}!` : 'Hi! What\'s your name?'
     return (
       <div>
-        <h1>Hello React Forms - part 2</h1>
+        <h1>Hello React Forms - part 3</h1>
         <p>
           {
-            `Now let's try to store the name user types inside the component's state, so we can re-use it!
-            Type your name and hit 'Say hi!' button to check the result.`
+            'Ok, you probably came here for more advanced example. Let\'s add some inputs to our form...'
           }
         </p>
         <form onSubmit={() => alert(alertMsg)}>
-          <label htmlFor="name">Your name:</label>
-          { /* Bind this.state.name to input's value and trigger our `handleInputChange` method on input change */}
-          <input type="text" id="name" value={this.state.name} onChange={this.handleInputChange} />
+          <div>
+            <label htmlFor="name">Your name:</label>
+            <input type="text" id="name" value={this.state.name} onChange={this.handleInputChange('name')} />
+          </div>
+          <div>
+            <label htmlFor="email">Your email:</label>
+            <input type="email" id="email" value={this.state.email} onChange={this.handleInputChange('email')} />
+          </div>
+          <div>
+            <label htmlFor="checkbox">
+              <input
+                type="checkbox"
+                id="checkbox"
+                value={this.state.checkbox}
+                onChange={this.handleInputChange('checkbox')}
+              />
+              Checkbox example
+            </label>
+          </div>
+          <div>
+            <label htmlFor="radio1">
+              <input
+                type="radio"
+                id="radio1"
+                name="radio"
+                value="option1"
+                checked={this.state.radio === 'option1'}
+                onChange={this.handleInputChange('radio')}
+              />
+              Option 1
+            </label><br />
+            <label htmlFor="radio2">
+              <input
+                type="radio"
+                id="radio2"
+                name="radio"
+                value="option2"
+                checked={this.state.radio === 'option2'}
+                onChange={this.handleInputChange('radio')}
+              />
+              Option 2
+            </label><br />
+            <label htmlFor="radio3">
+              <input
+                type="radio"
+                id="radio3"
+                name="radio"
+                value="option3"
+                checked={this.state.radio === 'option3'}
+                onChange={this.handleInputChange('radio')}
+              />
+              Option 3
+            </label>
+          </div>
+          <div>
+            <label htmlFor="date">Date example:</label>
+            <input type="date" id="date" value={this.state.date} onChange={this.handleInputChange('date')} />
+          </div>
           <button type="submit">Say hi!</button>
         </form>
       </div>
